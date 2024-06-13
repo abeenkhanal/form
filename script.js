1a21269a755e0d326a3bfa937c44eacc2b1f8ed9
Abeen Khanal <abeenkhanal16@gmail.com>
2024-06-13 09:53:07 +0545

    decorated

---
+++ script.js
@@ -0,0 +1,32 @@
+let fname="";
+let lname="";
+let email="";
+let phone="";
+let pass="";
+let pass2="";
+
+Firstname= ()=>{
+
+}
+
+
+Lastname=()=>{
+
+}
+
+
+
+Email=()=>{
+
+}
+
+Phone=(){
+    
+}
+
+password=(){
+
+}
+password2=(){
+    
+}
